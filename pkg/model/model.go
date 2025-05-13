c4e6a25c0f056cb108cf5f4ab31e36a15b438993
y-du <42994541+y-du@users.noreply.github.com>
2025-05-13 14:26:23 +0200

    add request and response models

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -35,3 +35,14 @@ func GetMonitorConnectionState(device ExtendedDevice) string {
 	}
 	return ""
 }
+
+type StatesRefreshRequestItem struct {
+	ID        string `json:"id"`
+	LMResult  int    `json:"lm_result"`
+	SubResult int    `json:"sub_result"`
+}
+
+type StatesRefreshResponseErrItem struct {
+	ID    string `json:"id"`
+	Error string `json:"error"`
+}
